35c92ce7811f972e998412a64a8aa15f9eb3ff63
Romka2005 <svetanok12@gmail.com>
2022-01-16 21:56:40 +0200

    резюме

---
+++ script.js
@@ -0,0 +1,4 @@
+let num1 = Number(prompt("Введите 1 число"));
+let num2 = Number(prompt("Введите 2 число"));
+alert(`Сумма: ${num1+num2} Разность: ${num1-num2} 
+Произведение: ${num1*num2} Частное: ${num1/num2}`);
